9f6a3cc4111871fcdf04169e8fb41d78f1664979
Dustin Kopta <dustinkopta@Dustins-iMac.local>
2023-03-28 14:23:25 -0600

    testing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is a NEW test.'));
+app.get('/', (req, res) => res.send('Pabst!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
